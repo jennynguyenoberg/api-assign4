8c45a842edd8c28c313bdfa60d38304a0449c671
Jenny Nguyen Öberg <116844306+jennynguyenoberg@users.noreply.github.com>
2023-01-21 22:21:32 +0100

    Adding button functionality and updated fetch data

--- bored.js
+++ bored.js
@@ -2,6 +2,10 @@ $(function () {
   const API_ADDRESS = "https://www.boredapi.com/api/activity/";
 
   function getRandomActivity() {
+    container.classList.replace("invisible", "visible");
+    header.classList.replace("visible", "invisible");
+    button.style.top = "75%";
+    button.style.transform = "translate(-50%, -50%) scale(1.4)";
     fetch(API_ADDRESS)
       .then((response) => {
         if (!response.ok) {
@@ -11,9 +15,9 @@ $(function () {
         }
       })
       .then((data) => {
-        $(".activity-name").text(data.activity);
-        $(".activity-category").text(data.type);
-        $(".participants").text(data.participants);
+        activity.innerHTML = `<span class="title">Activity: </span><span class="suggestion">${data.activity}</span>`;
+        type.innerHTML = `<span class="title">Type: </span><span class="suggestion">${data.type}</span>`;
+        participants.innerHTML = `<span class="title">Number of participants: </span><span class="suggestion">${data.participants}</span>`;
       })
       .catch(error => {
         $("main").append($("<div class='error'>").text("Something went wrong: " + error));
